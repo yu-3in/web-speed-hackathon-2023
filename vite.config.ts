56f1c054703d16d82c4bc1594cbbacb0aae127c4
yu-3in <misharv.y@gmail.com>
2024-03-10 21:38:02 +0900

    feat: setup bundle analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'node:path';
 
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 import { ViteEjsPlugin } from 'vite-plugin-ejs';
 import topLevelAwait from 'vite-plugin-top-level-await';
@@ -18,7 +19,7 @@ const getPublicFileList = async (targetPath: string) => {
   return publicFiles;
 };
 
-export default defineConfig(async () => {
+export default defineConfig(async ({ mode }) => {
   const videos = await getPublicFileList(path.resolve(publicDir, 'videos'));
 
   return {
@@ -31,6 +32,15 @@ export default defineConfig(async () => {
         output: {
           experimentalMinChunkSize: 40960,
         },
+        plugins: [
+          mode === 'analyze' &&
+            visualizer({
+              brotliSize: true,
+              filename: 'dist/stats.html',
+              gzipSize: true,
+              open: true,
+            }),
+        ],
       },
       target: 'es2015',
     },
