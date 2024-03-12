c18c208e9634c2e222d34605838e04e64930ecd0
yu-3in <misharv.y@gmail.com>
2024-03-12 16:48:31 +0900

    vite.configの設定で不要なプラグインを削除

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,11 @@
-import path from 'node:path';
-
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
-import { ViteEjsPlugin } from 'vite-plugin-ejs';
-import topLevelAwait from 'vite-plugin-top-level-await';
-import wasm from 'vite-plugin-wasm';
-
-import { getFileList } from './tools/get_file_list';
-
-const publicDir = path.resolve(__dirname, './public');
-const getPublicFileList = async (targetPath: string) => {
-  const filePaths = await getFileList(targetPath);
-  const publicFiles = filePaths
-    .map((filePath) => path.relative(publicDir, filePath))
-    .map((filePath) => path.join('/', filePath));
-
-  return publicFiles;
-};
 
 export default defineConfig(async ({ mode }) => {
-  const videos = await getPublicFileList(path.resolve(publicDir, 'videos'));
-
   return {
     build: {
-      assetsInlineLimit: 20480,
-      cssCodeSplit: false,
-      cssTarget: 'es6',
-      minify: false,
       rollupOptions: {
-        output: {
-          experimentalMinChunkSize: 40960,
-        },
         plugins: [
           mode === 'analyze' &&
             visualizer({
@@ -42,17 +16,7 @@ export default defineConfig(async ({ mode }) => {
             }),
         ],
       },
-      target: 'es2015',
     },
-    plugins: [
-      react(),
-      wasm(),
-      topLevelAwait(),
-      ViteEjsPlugin({
-        module: '/src/client/index.tsx',
-        title: '買えるオーガニック',
-        videos,
-      }),
-    ],
+    plugins: [react()],
   };
 });
